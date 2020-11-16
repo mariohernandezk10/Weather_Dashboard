75f0791fe76b788b994d881dd77399b4205eea4b
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-16 15:39:49 -0600

    added search bar

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // console.log(moment().format("LLL"));
 
-var apiKey = "2a75df114c3808db73405dca2f046501"
+const apiKey = "2a75df114c3808db73405dca2f046501"
 
+
+// var queryURL5 = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+
+// var queryURLUV = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+
+function getWeatherInfo (city){
+
+    let queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=2a75df114c3808db73405dca2f046501`
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    })
+}
+
+getWeatherInfo("austin")
