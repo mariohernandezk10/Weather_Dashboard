922099ccd15388fd6e55954ce82e3f19cee2cdad
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-17 15:24:02 -0600

    setting the local storage

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 // console.log(moment().format("LLL"));
 
-const apiKey = "2a75df114c3808db73405dca2f046501"
+// const apiKey = "2a75df114c3808db73405dca2f046501"
 
 
-// var queryURL5 = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
 
-// var queryURLUV = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2a75df114c3808db73405dca2f046501`
 
-function getWeatherInfo (city){
+let city = [];
 
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2a75df114c3808db73405dca2f046501`
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        // $.ajax({
-        //     url: queryURL
-        //     method: "GET"
-        // }).then(function(){
 
-        // })
-        console.log(response);
-        const temp = response.main.temp
-        const windSpeed = response.wind.speed
-    })
-}
+function getStorage() {
+	// Get the data stored at Local Storage
+	var getData = JSON.parse(localStorage.getItem("cityKey"));
+	console.log(getData);
+};
 
-getWeatherInfo("austin")
+getStorage();
+
+
+
+function setStorage() {
+	// Set the data stored at Local Storage
+
+    localStorage.setItem("cityKey", JSON.stringify(getData));
+};
+
+setStorage();
+
+
+
+$(".material-icons").on("click", function () {
+    // value of the search input
+    let newCity = $('#search').val().toLower();
+
+});
+
+
+
+var queryURL5 = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=2a75df114c3808db73405dca2f046501`
+
+let queryURLUV = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=2a75df114c3808db73405dca2f046501`
+
+// const temp = resp.main.temp
+// const humidity = resp.main.humidity
+// const windSpeed = resp.wind.speed
+
+
+
